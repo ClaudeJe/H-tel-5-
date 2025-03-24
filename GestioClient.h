03cc3ea29026bd70831286c30a2b1f8531f90e66
Djouwoua <157610846+ClaudeJe@users.noreply.github.com>
2025-03-24 23:09:20 +0100

    Add files via upload

---
+++ GestioClient.h
@@ -0,0 +1,10 @@
+#ifndef GestioClient_H_INCLUDED
+#define GestioClient_H_INCLUDED
+void FormulaireClient();
+void reservation();
+void commande();
+void AjouterClient();
+
+
+
+#endif // GestioClient_H_INCLUDED
