4e3fce8df25ecfc050b1d4a2b5a6023548afd242
Djouwoua <157610846+ClaudeJe@users.noreply.github.com>
2025-03-24 23:06:39 +0100

    Add files via upload

---
+++ Gestiopers.h
@@ -0,0 +1,11 @@
+#ifndef GestioPers_H_INCLUDED
+#define GestioPers_H_INCLUDED
+void Formulaire();
+void AfficheFormulaire();
+int AjouterEmployer();
+void AfficheBase();
+void MiseJourEmployer();
+void supprimeEtArchive();
+
+
+#endif // GestioPers_H_INCLUDED
